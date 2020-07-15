881790acc190fb495501693be20d0fd0c9fe3344
Sheepux <sheepux@live.com>
2020-07-15 02:05:58 +0200

    Hakuneko.download base

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# opensource.twitter.com
+# hakuneko.download
 
-Home of opensource.twitter.com
+Home of hakuneko.download
+
+## Installation instructions
+
+1. Install Ruby
+  - For windows https://rubyinstaller.org/downloads/
+
+2. Install Jekyl https://jekyllrb.com/docs/installation/
+
+# Test locally 
+
+bundle exec jekyll serve
+
+within vscode you can use https://marketplace.visualstudio.com/items?itemName=Dedsec727.jekyll-run
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "I See Dead People",
+        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/i-see-dead-people/",
+        "teaser": null
+      },{
+        "title": "New Site",
+        "excerpt":"I hate myself so much. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/new-site/",
+        "teaser": null
+      }]
